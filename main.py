0b93f1029a18d62040f2cbb11bb9502f5fb5ebbb
Trigun Joshi <iamtrigun@gmail.com>
2023-07-17 23:35:27 -0700

    scoreboard and bounce functionality added to the main

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from score import Score
 # Create the screen
 screen = Screen()
 screen.setup(width=800, height=600)
@@ -12,26 +13,34 @@ screen.tracer(0)
 left_paddle = Paddle((-350, 0))
 right_paddle = Paddle((350, 0))
 # Create the ball and make it move
-ball = Ball((0, 0))
-
+ball = Ball()
+score = Score()
 screen.listen()
 screen.onkey(left_paddle.go_up, "w")
 screen.onkey(left_paddle.go_down, "s")
 screen.onkey(right_paddle.go_up, "Up")
 screen.onkey(right_paddle.go_down, "Down")
 
-
-
-
-# Detect collision with wall and bounce
-# Detect collision with paddle
 # Detect when paddle misses
 # Keep Score
 
 is_game_on = True
 
 while is_game_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
+    # Detect collision with wall and bounce
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    # Detect collision with paddle
+    if ball.distance(right_paddle) < 50 and ball.xcor() > 320 or ball.distance(left_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    # Detect when paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        score.left_point()
+    if ball.xcor() < -380:
+        ball.reset_position()
+        score.right_point()
 screen.exitonclick()
